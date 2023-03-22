40ed69c8d23d15fc064eb6aaf3db7d3fb6d46fa7
hyeonayou <youhyeona99@gmail.com>
2023-03-22 23:04:06 +0900

    code test

---
+++ [프로그래머스 레벨0]/피자 나눠먹기(1).java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+
+        if(n % 7 == 0 )
+            return n/7 ;
+        else if(n % 7 >=1)
+            return ( n / 7) + 1;
+
+        return 0;
+    }
+}
